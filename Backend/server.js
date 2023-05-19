bceba65810f2f8f9796db0917ad608987b29e8c4
R.P Nethmini Tharuka <79852934+nethu99@users.noreply.github.com>
2023-05-19 22:54:55 +0530

    create server.js

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { connectDB } from "./database.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import InquiryRoute from "./Inquiries/routes/InquiryRoute.js";
+import ProjectRoute from "./Projects/routes/ProjectRoute.js";
+import SupplierRoute from "./Suppliers/routes/SupplierRoutes.js";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/inquiries", InquiryRoute);
+app.use("/projects", ProjectRoute);
+app.use("/suppliers", SupplierRoute);
+
+connectDB();
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
